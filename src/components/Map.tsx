3f7d570ac6afeb7d3c6bd5ffe6a6cdc33caa0b92
Kundan Gahalot <gahalotkundan@gmail.com>
2025-08-04 11:25:44 +0530

    map maker changes added

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,31 +1,67 @@
+// import "leaflet/dist/leaflet.css";
+// import type { Place } from "../api/Place";
+// import type { Map as LeafletMap } from "leaflet";
+// import { useEffect, useRef } from "react";
+// import { MapContainer, TileLayer, Marker } from "react-leaflet";
+
+// interface MapProps {
+//   place: Place | null;
+// }
+
+// export default function Map({ place }: MapProps) {
+//   const mapRef = useRef<LeafletMap | null>(null);
+
+//   useEffect(() => {
+//     if (mapRef.current && place) {
+//       mapRef.current.flyTo([place.latitude, place.longitude]);
+//     }
+//   }, [place]);
+//   return (
+//     <MapContainer
+//       ref={mapRef}
+//       center={[40.7, -74]}
+//       zoom={12}
+//       scrollWheelZoom
+//       className="h-full"
+//     >
+//       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+//       {place && <Marker position={[place.latitude, place.longitude]} />}
+//     </MapContainer>
+//   );
+// }
+
 import "leaflet/dist/leaflet.css";
+import "../utils/fixLeafletIcon";
 import type { Place } from "../api/Place";
-import type { Map as LeafletMap } from "leaflet";
-import { useEffect, useRef } from "react";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { useEffect } from "react";
+import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
 
 interface MapProps {
   place: Place | null;
 }
 
-export default function Map({ place }: MapProps) {
-  const mapRef = useRef<LeafletMap | null>(null);
+function FlyToMarker({ place }: { place: Place }) {
+  const map = useMap();
 
   useEffect(() => {
-    if (mapRef.current && place) {
-      mapRef.current.flyTo([place.latitude, place.longitude]);
+    if (place) {
+      map.flyTo([place.latitude, place.longitude], 13);
     }
-  }, [place]);
+  }, [place, map]);
+
+  return <Marker position={[place.latitude, place.longitude]} />;
+}
+
+export default function Map({ place }: MapProps) {
   return (
     <MapContainer
-      ref={mapRef}
       center={[40.7, -74]}
       zoom={12}
       scrollWheelZoom
       className="h-full"
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {place && <Marker position={[place.latitude, place.longitude]} />}
+      {place && <FlyToMarker place={place} />}
     </MapContainer>
   );
 }
